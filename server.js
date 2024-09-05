2d8bcd2b1833214f7b99622211a21d851690381c
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2024-09-04 22:14:37 -0300

    agregado servidor adicional para resolver nombres

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const app = express();
-const port = 3000;
+const port_server_1 = 3000; // Seria el "root"
+const port_server_2 = 3001;
 
 // Tabla de nombres (simula la gestión de un espacio de nombres)
-let tablaNombresLocal = {
+const tablaNombresServer1 = {
     "/sistema/archivos/documentos/reporte.txt": "http://servidor-archivos/documentos/reporte.txt",
     "/procesos/12345": "http://servidor-procesos/12345"
 };
@@ -12,13 +13,14 @@ let tablaNombresLocal = {
 app.get('/resolver', async (req, res) => {
     const nombre = req.query.nombre;
 
-    if (tablaNombresLocal[nombre]) {
-        res.json({ referencia: tablaNombresLocal[nombre] });
+    if (tablaNombresServer1[nombre]) {
+        res.json({ referencia: tablaNombresServer1[nombre] });
+        console.log('Nombre resuelto en servidor 1')
     }
     // Si no encuentra en su tabla de nombres, intenta resolver en otro servidor
     else {
         try {
-            const respuesta = await fetch(`http://otro-servidor-nombres:3000/resolver?nombre=${nombre}`).then(res => res.json());
+            const respuesta = await fetch(`http://localhost:3001/resolver?nombre=${nombre}`).then(res => res.json());
             res.json({ referencia: respuesta.referencia });
         } catch (error) {
             res.status(404).json({ error: "Recurso no encontrado en ningún servidor." });
@@ -26,6 +28,28 @@ app.get('/resolver', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Servidor de nombres escuchando en el puerto ${port}`);
+app.listen(port_server_1, () => {
+    console.log(`Servidor de nombres escuchando en el puerto ${port_server_1}`);
 });
+
+const tablaNombresServer2 = {
+    "/servicios/desconocido": "http://servidor-servicios/desconocido"
+};  
+
+app.get('/resolver', async (req, res) => {
+    const nombre = req.query.nombre;
+
+    if (tablaNombresServer2[nombre]) {
+        res.json({ referencia: tablaNombresServer2[nombre] });
+        console.log('Nombre resuelto en servidor 2');
+    }
+    // Si no encuentra en su tabla de nombres, intenta resolver en otro servidor
+    else {
+        res.status(404).json({ error: "Recurso no encontrado en ningún servidor." });
+    }
+})
+
+app.listen(port_server_2, () => {
+    console.log(`Servidor de nombres escuchando en el puerto ${port_server_2}`);
+});
+    
